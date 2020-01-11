ec6ee2181dfeb3bf7fb745457272f9240c932e8b
Andre Barreto <52931189+andrelbarreto@users.noreply.github.com>
2020-01-10 22:36:54 -0600

    Third push, with all files needed in progress

---
+++ README.md
@@ -0,0 +1,44 @@
+# Andre Barreto Team PDF Creator 1.0
+This is a Git Hub repository for the NU Coding Bootcamp Team Site Generator Homework for Week 10.
+
+![profile pdf screenshot](/assets/images/background_bio.jpg)
+![node js run gif](/assets/images/10-OOP-homework-demo-1.png)
+
+# Setup
+
+Once logged in with [GitHub account](https://github.login/), clone the repo locally using link:
+
+```sh
+  git clone https://github.com/andrelbarreto/TeamPdfCreator
+```
+
+Also see: [Cloning a repository](https://help.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository).
+
+
+# Usage
+
+This application allows a user to generate a webpage that displays information regarding members of a team based on prompts and stored user inputs using the command-line, the team can have add members and each can be assigned a position.
+
+A project manager, department head or developer lead can use the application to generate basic team details quickly for a website that can be referenced by stakeholders or boards without having to navigate to external profiles provided by each team member. 
+
+
+When the command line prompts are complete, a team.html page will save to "output" folder.
+
+The generated page will include cards for each member of the team with:
+- name
+- title
+- id
+- role (manager, intern, engineer)
+- depending on the role, github username, school, or office number
+
+# Technologies
+
+This project was built using:
+
+  - [Inquirer 7.0.1](https://www.npmjs.com/package/inquirer)
+  - [Jest 24.8.0](https://www.npmjs.com/package/jest)
+
+
+# License
+
+This project uses the MIT License. See the full details here: https://choosealicense.com/licenses/mit/ 
---
+++ app.js
@@ -0,0 +1,238 @@
+//app.js runs the application on node js
+//the folder lib contains classes for each employee type
+//template contains the html templates for each type
+//output is where the rendered file(s) are saved
+
+// ### User input
+// The project must prompt the user to build an engineering team. An engineering
+// team consists of a manager, and any number of engineers and interns.
+
+// The project must generate a `team.html` page in the `output` directory, that displays a nicely formatted team roster. Each team member should display the following in no particular order:
+//   * Name
+//   * Role
+//   * ID
+//   * Role-specific property (School, link to GitHub profile, or office number)
+
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employee = require("./lib/Employee");
+const manager = require("./lib/Manager");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+
+//array of data that will be input into arrayHTML
+let arrayAnswers = [];
+
+//array of html that will be created from inputs and collect inputs
+let arrayHTML = [];
+
+//array of html that will be written to the final output team_index.html
+let arrayHTMLFinal = [];
+
+let arrayHTMLFinalTemplate = [];
+
+
+function initiate() {
+
+    console.log(`Let's build your team. Please follow the prompts and answer questions below:`);
+
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Select this team member's role:",
+        choices: ["manager", "engineer", "intern"]
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Enter this team member's name:"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter id:"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter email address of this team member following format (name@domain.com):"
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Enter GitHub username:",
+        when: (data) => data.type === 'engineer',
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter name of school:",
+        when: (data) => data.type === 'intern',
+      },
+      {
+        type: "input",
+        name: "officenumber",
+        message: "Enter office number:",
+        when: (data) => data.type === 'manager',
+      },
+      {
+        type: "confirm",
+        name: "AddMember",
+        message: "Add another team member? "
+      }
+    ])
+
+        //then with the input of position and extra question per type
+        .then(function(data) {
+
+          // console.log(data);
+          // console.log(data.type);
+
+          if (data.role === 'manager') {
+
+            //creates a new manager using manager class
+            const newManager = new manager(data.name, data.id, data.email, data.officenumber);
+            
+            //push newManager to answers array
+            arrayAnswers.push(newManager);
+
+            arrayHTML = [];
+
+            //read contents of html file
+            fs.readFile("./templates/template_manager.html", "utf8", function(err, data) {
+              if (err) { throw err;}
+
+              //push the information pulled from the template into an array of HTML data
+              arrayHTML.push(data);
+
+              //return the HTML with the placeholders replaced by newManager data
+              arrayHTML = arrayHTML.map(arrayHTML => {
+                return arrayHTML.replace(/{{ id }}/g, newManager["id"]).replace(/{{ email }}/g, newManager["email"]).replace(/{{ officenumber }}/g, newManager["officeNumber"]).replace(/{{ name }}/g, newManager["name"]).replace(/{{ type }}/g, newManager.getRole());
+                
+              });
+
+              //push updated html into array final that will hold all updated html data
+              arrayHTMLFinal.push(arrayHTML);
+
+            // end read file
+            });
+
+          // end if statement
+          }
+
+          if (data.role === 'intern') {
+            
+            //creates new intern entry using intern class
+            const newIntern = new intern(data.name, data.id, data.email, data.school);
+
+            arrayAnswers.push(newIntern);
+
+            arrayHTML = [];
+
+            //read contents of html file
+            fs.readFile("./templates/template_intern.html", "utf8", function(err, data) {    
+              if (err) { throw err; }
+              
+              //push the information pulled from the template into an array of HTML data
+              arrayHTML.push(data);
+
+              //return the HTML with the placeholders replaced by newIntern data
+              arrayHTML = arrayHTML.map(arrayHTML => {
+                return arrayHTML.replace(/{{ id }}/g, newIntern["id"]).replace(/{{ email }}/g, newIntern["email"]).replace(/{{ school }}/g, newIntern["school"]).replace(/{{ name }}/g, newIntern["name"]).replace(/{{ type }}/g, newIntern.getRole());
+                
+              });
+
+              //push updated html into array that will hold all updated html data
+              arrayHTMLFinal.push(arrayHTML);
+
+            // end read file
+            });
+
+          // end if statement
+          }
+
+          if (data.role === 'engineer') {
+           
+            //creates a new engineer using engineer class
+            const newEngineer = new engineer(data.name, data.id, data.email, data.username);
+
+            //push newEngineer to answers array
+            arrayAnswers.push(newEngineer);
+            
+            arrayHTML = [];
+
+            //read contents of html file
+            fs.readFile("./templates/template_engineer.html", "utf8", function(err, data) {
+              
+              if (err) {
+                throw err;
+              }
+              
+              //push the information pulled from the template into an array of HTML data
+              arrayHTML.push(data);
+
+              //return the HTML with the placeholders replaced by newEngineer data
+              arrayHTML = arrayHTML.map(arrayHTML => {
+                return arrayHTML.replace(/{{ id }}/g, newEngineer["id"]).replace(/{{ email }}/g, newEngineer["email"]).replace(/{{ username }}/g, newEngineer["github"]).replace(/{{ name }}/g, newEngineer["name"]).replace(/{{ type }}/g, newEngineer.getRole());
+                
+              });
+
+              //push updated html into array final that will hold all updated html data
+              arrayHTMLFinal.push(arrayHTML);
+            
+            });
+
+          } 
+          
+          if (data.AddMember === false) {
+
+            writeFile();
+
+          } else {
+
+            initiate();
+
+          }
+      
+    //end inquirer
+     }); 
+
+}
+
+function writeFile() {
+
+    //read contents of shell html file
+    fs.readFile("./templates/template_index.html", "utf8", function(err, data) {
+              
+      if (err) {
+        throw err;
+      }
+
+      arrayHTMLFinalTemplate.push(data);
+
+      //return the HTML with the placeholders replaced by newteam data
+      arrayHTMLFinalTemplate = arrayHTMLFinalTemplate.map(arrayHTMLFinalTemplate => {
+        return arrayHTMLFinalTemplate.replace(/{{ newteam }}/g, arrayHTMLFinal);
+      });
+
+      arrayHTMLFinalTemplate = arrayHTMLFinalTemplate.join("");
+
+      // write to new file in output folder
+      fs.writeFile("./output/team_index.html", arrayHTMLFinalTemplate, function(err) {
+      
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+      
+        });
+
+    });
+
+  }
+
+//run when app is initialized
+initiate();
